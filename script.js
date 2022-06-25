5b6144df9b4ab62530d0902da6e5f8894a7c7709
davisnoah <davisnoah8079@gmail.com>
2022-06-25 10:50:32 -0500

    Used querySelector; Added Comments; Used rem not px;

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const frame = document.getElementsByClassName("frame")[0];
-const stringLeft = document.getElementsByClassName("string-left")[0];
-const stringRight = document.getElementsByClassName("string-right")[0];
-const pin = document.getElementsByClassName("pin")[0];
+// Selects frame elements from DOM
+const frame = document.querySelector(".frame");
+const stringLeft = document.querySelector(".string-left");
+const stringRight = document.querySelector(".string-right");
+const pin = document.querySelector(".pin");
 
-//STRING LENGTH CALCULATION
-const pinTop = parseInt(getComputedStyle(pin).getPropertyValue('top'), 10);
-const frameWidth = parseInt(getComputedStyle(frame).getPropertyValue('width'), 10);
-const stringOffset = parseInt(getComputedStyle(stringLeft).getPropertyValue('left'), 10);
+// gets css values of components
+const pinTop = parseInt(getComputedStyle(pin).getPropertyValue('top'));
+const frameWidth = parseInt(getComputedStyle(frame).getPropertyValue('width'));
+const stringOffset = parseInt(getComputedStyle(stringLeft).getPropertyValue('left'));
 
+// calculates frame string vertical and horizontal componentxs
 const stringVertComp = (pinTop * -1);
 const stringHorizComp = (frameWidth/2 - stringOffset);
 
+// calculates string width
 const stringWidth = Math.sqrt(stringHorizComp**2 + stringVertComp**2).toFixed(2) + 3;
 
 stringLeft.style.setProperty('width', `${stringWidth}px`);
 stringRight.style.setProperty('width', `${stringWidth}px`);
 
-//STRING ANGLE CALCULATION
+// STRING ANGLE CALCULATION
 const stringAngleLeft = Math.atan(stringVertComp/stringHorizComp) * 180/Math.PI * -1;
 const stringAngleRight = Math.atan(stringVertComp/stringHorizComp) * 180/Math.PI;
 
